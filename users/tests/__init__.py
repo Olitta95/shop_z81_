b71c4d7c8c25cd33c459843871bfa2fac3878510
olitta95 <lesnikovaolga1995@gmail.com>
2023-03-28 19:46:00 +0300

    initial catalog add

--- users/tests/__init__.py
+++ users/tests/__init__.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+import pytest
+from rest_framework.test import APITestCase, APIClient
+from django.shortcuts import reverse
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import get_user_model
