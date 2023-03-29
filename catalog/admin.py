e58a00c1e5a53948cecdf94e1de280693d1f5dec
olitta95 <lesnikovaolga1995@gmail.com>
2023-03-29 23:39:57 +0300

    initial catalog add

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Promocode, Producer, Discount
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Promocode)
+admin.site.register(Producer)
+admin.site.register(Discount)
