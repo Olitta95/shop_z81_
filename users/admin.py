29cd297fa2db2a4cb256df9a52e2d63071b85e0f
olitta95 <lesnikovaolga1995@gmail.com>
2023-03-31 20:47:35 +0300

    initial catalog add admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from users.models import CustomUser
 
 # Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email','phone_number','is_active','is_staff','is_superuser',)
+    search_fields = ('email','phone_number', )
+
+admin.site.register(CustomUser,CustomUserAdmin)
